8e6c3b726aced519c59013c9d75301c4dced72a3
Carmelo Carnamucio <carnamuciocarmelo@gmail.com>
2020-10-24 18:01:56 +0200

    aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -8,8 +8,12 @@ var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"]
 //console.log(listaCognomi);
 
 //Lista Cognomi da stampare
+ //Lista ordinata alfabeticamente
 var elementoLista = document.getElementById("lista")
-var elementPosizione = document.getElementById("posizione");
+var elementoPosizioneOn = document.getElementById("posizione");
+ //Lista non ordinata alfabeticamente
+var elementoLista2 = document.getElementById("lista2");
+var elementoPosizioneUn = document.getElementById("posizione2");
 
 //Variabile di appoggio
 var mailTrovata = false;
@@ -24,30 +28,40 @@ while (cognomeUtente.length === 0) {
 //Verifico che il cognome Utente sia tra quelli nella lista
 for (var i = 0; i < listaCognomi.length; i++) {
 
-  //Se la mail é in lista stampo un messaggio
+  //Se il cognome é in lista stampo
   if (listaCognomi[i] === cognomeUtente) {
   var mailTrovata = true;
   //console.log("il tuo cognome é in lista")
 }
 
-//Stampo la lista
 //console.log("stampo lista: " + listaCognomi[i])
 }
 
+//Se il cognome non è in lista lo aggiungo e stampo
 if (mailTrovata === false) {
   listaCognomi.push(cognomeUtente);
   //console.log("lista cognomi aggiornata: " + listaCognomi)
 }
 
-//Stampa la lista ordinata alfabeticamente
+//Stampa la lista non ordinata alfabeticamente
 for (var i = 0; i < listaCognomi.length; i++) {
 
-listaCognomi = listaCognomi.sort();
-elementoCorrente = elementoLista.innerHTML
-elementoLista.innerHTML = elementoCorrente + "<li>" + (i + 1) + " " + listaCognomi[i] + "</li>";
+ elementoLista2.innerHTML = elementoLista2.innerHTML + "<li>" + " " + listaCognomi[i] + "</li>";
 
 }
 
-//Stampa il numero dove è posizionato l'utente
+//Stampa il numero dove è posizionato l'utente non ordinato alfabeticamente
+var listaCognomiNonOrdinata = listaCognomi.indexOf(cognomeUtente) + 1;
+elementoPosizioneUn.innerHTML = listaCognomiNonOrdinata;
+
+//Stampa la lista ordinata alfabeticamente
+for (var i = 0; i < listaCognomi.length; i++) {
+
+ listaCognomi = listaCognomi.sort();
+ elementoLista.innerHTML = elementoLista.innerHTML + "<li>" + " " + listaCognomi[i] + "</li>";
+
+}
+
+//Stampa il numero dove è posizionato l'utente ordinato alfabeticamente
 var listaCognomiOrdinata = listaCognomi.indexOf(cognomeUtente) + 1;
-elementPosizione.innerHTML = listaCognomiOrdinata;
+elementoPosizioneOn.innerHTML = listaCognomiOrdinata;
